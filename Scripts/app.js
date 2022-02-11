c2240978455d4fb346c21eeebc00485916bbee99
Nimsith <nimsith32@gmail.com>
2022-02-11 01:09:55 -0500

    Creating variables for all strings

---
+++ Scripts/app.js
@@ -0,0 +1,19 @@
+/**Variable to store string values of the about html page */
+var about;
+about = "Hi, My Name is Nimsith Fernandopulle , I am currently enrolled in the program Computer Programming and Analysis. The reason why I chose the IT field as a career path was because I always loved how technology was used in automotive industry. Specially how IT is used in F1 facinated me where before during after each race they process over 20GB of data every weeked which also makes it the most advanced motoracing sport in the world";
+
+/**Variable to store string values of the Contact html page */
+var contacth;
+var contacte;
+var contactt;
+contacth ="Contact us"; 
+contacth = "Email: nimsith.fernandopulle@dcmail.ca";
+contactt = "Tel  : 6476364788";
+
+/**Variable to store string values of the Contact html page */
+var contacth;
+var contacte;
+var contactt;
+contacth ="Contact us"; 
+contacth = "Email: nimsith.fernandopulle@dcmail.ca";
+contactt = "Tel  : 6476364788";
